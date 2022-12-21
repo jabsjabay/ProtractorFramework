422142d80b3db519959e340b57a0733fcf46587a
Jabs Jabay <j.jabay@arcanys.com>
2022-12-21 13:27:26 +0800

    This is just a test commit

--- conf/conf.js
+++ conf/conf.js
@@ -14,7 +14,7 @@ exports.config = {
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome' 
   },
 
   // Framework to use. Jasmine is recommended.
@@ -26,7 +26,7 @@ exports.config = {
 
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
+    defaultTimeoutInterval: 30000 
   },
 
   // Setup the report before any tests start
